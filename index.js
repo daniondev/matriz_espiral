e1555607f49363b35720b56bf78b7c6f921077c1
Daniel Arroyo Martínez <daniondev@gmail.com>
2022-07-25 21:56:16 -0400

    Identación
    Ordenar el código desde GitHub

--- index.js
+++ index.js
@@ -8,61 +8,61 @@ function solution(s) {
     let inicioFila = 0;
     let inicioColumna = 0;
     while(newMatriz.length < cantidad){
-            let izq = [];
-            let ultimo = [];
+        let izq = [];
+        let ultimo = [];
         for(let i = inicioFila; i < sFilas; i++){
             for(let j = inicioColumna; j < sColumnas; j++){
                 if(i == inicioFila){
                   newMatriz.push(s[i][j]);
                 }else if(i > inicioFila && i < sFilas - 1){
-                      if(j == inicioColumna){
-                                              izq.push(s[i][j])                
-                        }
+                    if(j == inicioColumna){
+                        izq.push(s[i][j])                
+                    }
                       
-                      if(j == sColumnas - 1){
-                          newMatriz.push(s[i][j])                
+                    if(j == sColumnas - 1){
+                        newMatriz.push(s[i][j])                
                     }
                 }else{
-                          ultimo.push(s[i][j]);
+                    ultimo.push(s[i][j]);
                 }
             }
         }
             
-            newMatriz.push(...ultimo.reverse());
-            newMatriz.push(...izq.reverse());
+        newMatriz.push(...ultimo.reverse());
+        newMatriz.push(...izq.reverse());
       
-          inicioFila++;
-            inicioColumna++;
-          sFilas--;
-          sColumnas--;
+        inicioFila++;
+        inicioColumna++;
+        sFilas--;
+        sColumnas--;
     }
     
     return newMatriz;
-  }
+}
   
-  const matrix1 =
-  [
-   [ 1, 2, 3 ],
-   [ 8, 9, 4 ],
-   [ 7, 6, 5 ]
-  ]
-  
-  const matrix2 =
-  [
-   [ 1, 2, 3, 4 ],
-   [ 12, 13, 14, 5 ],
-   [ 11, 16, 15, 6 ],
-   [ 10, 9, 8, 7 ]
-  ]
-  
-  const matrix3 =
-  [
-   [ 1, 2, 3, 4, 5 ],
-   [ 16, 17, 18, 19, 6 ],
-   [ 15, 24, 25, 20, 7 ],
-   [ 14, 23, 22, 21, 8 ],
-   [ 13, 12, 11, 10, 9 ]
-  ]
+const matrix1 =
+[
+    [ 1, 2, 3 ],
+    [ 8, 9, 4 ],
+    [ 7, 6, 5 ]
+]
 
-  console.log(matrix3);
-  console.log(solution(matrix3));
+const matrix2 =
+[
+    [ 1, 2, 3, 4 ],
+    [ 12, 13, 14, 5 ],
+    [ 11, 16, 15, 6 ],
+    [ 10, 9, 8, 7 ]
+]
+
+const matrix3 =
+[
+    [ 1, 2, 3, 4, 5 ],
+    [ 16, 17, 18, 19, 6 ],
+    [ 15, 24, 25, 20, 7 ],
+    [ 14, 23, 22, 21, 8 ],
+    [ 13, 12, 11, 10, 9 ]
+]
+
+console.log(matrix3);
+console.log(solution(matrix3));
